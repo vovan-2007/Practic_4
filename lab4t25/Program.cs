252c73f97e29ad30d0a7321151a9e740cc76bc70
amogus <110690851+vovan-2007@users.noreply.github.com>
2025-06-04 11:07:16 +0400

    Add files via upload

---
+++ lab4t25/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace lab4t25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите n (1<n<=10): ");
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int factorial = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                factorial *= i;
+                sum += factorial;
+            }
+            Console.WriteLine($"Сумма факториалов: {sum}");
+            Console.WriteLine("Нажмите любую клавишу для выхода...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ lab4t26/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace lab4t26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите n (1<n<=10): ");
+            int n = int.Parse(Console.ReadLine());
+            double sum = 1;
+            double factorial = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                factorial *= i;
+                sum += 1.0 / factorial;
+            }
+            Console.WriteLine($"Сумма ряда: {sum}");
+            Console.WriteLine("Нажмите любую клавишу для выхода...");
+            Console.ReadKey();
+        }
+    }
+}
